f71a0f275f3b07e76cb1e00533d03016a0763fce
Arunachalamvk <arunachalamvik@gmail.com>
2020-08-26 11:25:56 +0530

    deployed

---
+++ src/hooks/useVideos.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+import youtube from "../apis/youtube";
+const KEY = "AIzaSyB9YJHfOQLzKrUXR2iINv1qDk42OeVQ6tI";
+
+const useVideos = (defaultSearchTerm) => {
+  const [videos, setVideos] = useState([]);
+  useEffect(() => {
+    search(defaultSearchTerm);
+  }, [defaultSearchTerm]);
+
+  const search = async (term) => {
+    const response = await youtube.get("/search", {
+      params: {
+        q: term,
+        part: "snippet",
+        maxResults: 5,
+        type: "video",
+        key: KEY,
+      },
+    });
+    setVideos(response.data.items);
+  };
+
+  return [videos, search];
+};
+export default useVideos;
